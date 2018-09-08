4886bb0eec508020912e74e927eb00e16e5d6b2f
gbagrowski <gbagrowski@fixiti.com>
2018-09-08 18:39:04 -0400

    Created ChatGenesisBlock. However, before I start on it, I'm reading up on how it's authenticated (miners, etc, and the 51% rule).

---
+++ app/Models/Chat/ChatGenesisBlock.php
@@ -0,0 +1,13 @@
+<?php
+namespace Models\Chat;
+
+/**
+ * Class ChatGenesisBlock
+ *
+ * @package Models\Chat
+ *
+ * Note: Use blockchain concept to store the data, using
+ */
+class ChatGenesisBlock {
+    // ?
+}
